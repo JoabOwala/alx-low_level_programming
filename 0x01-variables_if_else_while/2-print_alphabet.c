0d0b937bb78f9dcd62a7cdfb97e3d0b49cc79e48
JoabOwala <owalajoab@gmail.com>
2022-06-02 23:43:43 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*alphabet 
+*Return: 0
+*/
+int main(void)
+{
+char alp;
+for(alp='a';alp<='z';alp++)
+{
+putchar(alp);
+}
+putchar ('\n');
+return (0);
+}
