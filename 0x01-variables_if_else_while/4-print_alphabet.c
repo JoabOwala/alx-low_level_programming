3a01b296f6b30550366b4c09dea0d84f43e40b5e
JoabOwala <owalajoab@gmail.com>
2022-06-03 01:12:59 -0700

    alphabet 3

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+*Return: 0
+*/
+int main(void)
+{
+char ad;
+for (ad = 'a';ad<='z';ad++)
+{
+if(ad=='q')
+continue;
+else if(ad=='e')
+continue;
+putchar (ad);
+}
+putchar('\n');
+return(0);
+}
