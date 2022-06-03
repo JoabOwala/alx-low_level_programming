17282fbdb06b0c8e3c0b8518423d4ec65772b14e
JoabOwala <owalajoab@gmail.com>
2022-06-03 01:51:16 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: 0
+*/
+int main(void)
+{
+int n;
+char a;
+
+for (n='0';n='9';n++)
+{
+putchar(n);
+}
+for(a='a';a<='f';a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
