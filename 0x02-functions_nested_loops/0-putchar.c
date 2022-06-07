a175758aa41be4705e53802c174ade0d7bdc6b18
JoabOwala <owalajoab@gmail.com>
2022-06-07 01:30:21 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  char *ch = "_putchar";
+  while (*ch)
+    {
+      _putchar(*ch);
+      ch++;
+    }
+  _putchar('\n');
+
+  return (0);
+}
