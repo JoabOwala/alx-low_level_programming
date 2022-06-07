983439feeca10f07958ac3f4770ca6272dd2ce63
JoabOwala <owalajoab@gmail.com>
2022-06-07 05:09:18 -0700

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+  int myn, ter;
+  ter = 0;
+
+  while (ter < 10)
+    {
+      for (myn = 'a'; myn <= 'z'; n++)
+	{
+	  _putchar(n);
+	}
+      ter++;
+      _putchar('n');
+    }
+}
