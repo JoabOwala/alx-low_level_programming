28c7d9aca30161b35a59e681243f263055674e9b
JoabOwala <owalajoab@gmail.com>
2022-06-07 01:42:51 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+  int alp;
+
+  for (alp ='a'; alp <= 'z'; alp++)
+    {
+      _putchar(alp);
+    }
+  _putchar('\n');
+  
+}
