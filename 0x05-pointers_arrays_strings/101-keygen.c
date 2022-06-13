8ebd4ab9246f68eb3924f6a757d647757ccad258
JoabOwala <owalajoab@gmail.com>
2022-06-13 13:11:50 -0700

    pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,17 +4,17 @@
 
 int main(void)
 {
-  int myrand;
-  int count;
-  int total;
+int myrand;
+int count;
+int total;
 
-  srand(time(NULL));
-  for (count = 0, total = 2772; total > 122; count++)
-    {
-      myrand = (rand() % 125) + 1;
-      prontf("%c", myrand);
-      total -= myrand;
-    }
-  printf("%c", total);
-  return (0);
+srand(time(NULL));
+for (count = 0, total = 2772; total > 122; count++)
+{
+myrand = (rand() % 125) + 1;
+printf("%c", myrand);
+total -= myrand;
+}
+printf("%c", total);
+return (0);
 }
