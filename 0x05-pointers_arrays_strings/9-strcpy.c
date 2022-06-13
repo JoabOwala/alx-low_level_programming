b0b1520d18d7f07c0ed131ddf9b9d7d9664d0c15
JoabOwala <owalajoab@gmail.com>
2022-06-13 13:21:46 -0700

    pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *
  */
-void print_array(int *a, int n)
+
+char *_strcpy(char *dest, char *src)
 {
-  int i;
-  n = 5;
-  for(i = 0; i < n-1; i++)
-    {
-      printf("%d, ", a[i]);
-    }
-  printf("%d", a[n-1]);
-  printf("\n");
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+
+return (dest);
 }
