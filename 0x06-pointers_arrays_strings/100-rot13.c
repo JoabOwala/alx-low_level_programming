a29dc8f8c0f90eff59b0cb91428b0cf57dfed2f2
JoabOwala <owalajoab@gmail.com>
2022-06-16 06:27:39 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*
+*/
+char *rot13(char *s)
+{
+int sCount, rotate;
+char r1[] = {'a', 'b', 'c' ,'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w','x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+char r2[] ={'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+for (sCount = 0; s[sCount] != '\0'; sCount++)
+{
+for (rotate = 0; rotate < 53; rotate++)
+{
+if (r1[rotate] == s[sCount])
+{
+s[sCount] = r2[rotate];
+break;
+}
+}
+}
+return (s);
+}
