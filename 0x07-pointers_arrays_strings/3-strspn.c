e29016bc353af094176dee5601d4851418d4db9f
JoabOwala <owalajoab@gmail.com>
2022-06-20 03:01:38 -0700

    more pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,7 +6,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 int a, b, counter = 0;
-for (a =0; s[a] != '\0'; i++)
+for (a =0; s[a] != '\0'; a++)
 if (counter != a)
 break;
 for (b = 0; accept[b] != '\0'; b++)
