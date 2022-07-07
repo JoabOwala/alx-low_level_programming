e77447168e47a546d06b1ba598681eea436c28a3
JoabOwala <owalajoab@gmail.com>
2022-07-07 07:02:52 -0700

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,7 +12,7 @@ void print_a_integer(char *separator, va_list args);
 void print_a_float(char *separator, va_list args);
 void print_a_char_ptr(char *separator, va_list args);
 
-typedef strct format_types
+typedef struct format_types
 {
   char *identifier;
   void (*f)(char *separator, va_list args);
